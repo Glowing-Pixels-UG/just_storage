fe8d0fb386d9f87bbee14008dd0d2cdf6e2adf46
Damir <damir@laika.baikonur.glpx.pro>
2026-01-26 01:36:21 +0100

    tests(integration): add wrapper for use_cases integration tests

---
+++ rust/tests/integration.rs
@@ -0,0 +1,8 @@
+#[path = "integration/use_cases/multi_object_operations.rs"]
+mod multi_object_operations;
+#[path = "integration/use_cases/namespace_validation.rs"]
+mod namespace_validation;
+#[path = "integration/use_cases/object_lifecycle.rs"]
+mod object_lifecycle;
+#[path = "integration/use_cases/storage_class_behavior.rs"]
+mod storage_class_behavior;
